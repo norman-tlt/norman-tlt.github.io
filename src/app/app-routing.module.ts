5ba5f1ad81482157546b2265af403db1c4ea7863
norman-tlt <norman.argiolas@telit.com>
2021-07-06 18:44:19 +0200

    WIP - skeleton page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ExploreComponent} from "./components/explore/explore.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ExploreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
